86045c7300c3ca0a8ac1c70cfb0b45a69e7bdf70
Ataud-dayyan <ghazalatauddayyan@gmail.com>
2025-06-01 16:04:13 +0100

    Employee App

---
+++ Models/EditDepartmentViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+ 
+namespace Presentation.Models;
+
+    public class EditDepartmentViewModel
+    {
+        public string? Name { get; set; }
+
+        [MaxLength(500, ErrorMessage = "Description cannot exceed 500 characters.")]
+        public string? Description { get; set; }
+    }
+
