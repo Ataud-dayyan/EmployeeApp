5c568f775dcdb530f7e943e6d61c1bbd10b47cba
Ataud-dayyan <ghazalatauddayyan@gmail.com>
2025-06-02 20:24:51 +0100

    Updated code

--- obj/Debug/net9.0/Presentation.AssemblyInfo.cs
+++ obj/Debug/net9.0/Presentation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5373a4d97fddbabe5fa701cad80a3f27594caa5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfcf686d9a38855c1f56024f738a9770c404411d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
